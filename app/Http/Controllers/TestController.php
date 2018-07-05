71faf19c82579defe85750b6d6f1d03fef0160b1
vagrant <vagrant@homestead>
2018-07-05 09:23:39 +0000

    Initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function test(){
+        echo 'aa';
+        $val = getenv('APP_DEBUG');
+        dd($val);
+    }
+}
